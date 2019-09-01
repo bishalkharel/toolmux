0215c697905e3989f415be5aac20d6d5e797b21f
bishalkharel <49745633+bishalkharel@users.noreply.github.com>
2019-09-01 08:06:13 +0545

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+pkg install python
+pkg install python2 
+pkg install nmap
+pkg install openssh
+pkg install figlet
+pkg install python
+pkg install python2 
+pip install -r requirements.txt
+
+echo   "=====Awesome your requirements are downloaded======="
+              
+ echo   " © Toolmux 2019"
+
+
+echo   "Developed by:bishal kharal"
+echo   "INSTAGRAM:bsal_kharel"
+ echo  "TWITTER:Bishal kharel"
